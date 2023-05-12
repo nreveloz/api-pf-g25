2fe3ec37b223cd8b43fb9e9bcdc00cf844269944
Nataly Revelo <natazarama@hotmail.com>
2023-05-12 17:40:53 -0400

    Combined filters implementation by query params

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -4,6 +4,7 @@ const getBookDetailById = require('../controllers/getBookDetailById');
 const getBookByTitleLike = require('../controllers/getBookByTitleLike');
 const getBooksByGenre = require('../controllers/getBooksByGenre');
 const getBooksByAuthor = require('../controllers/getBooksByAuthor');
+const getBooksByCombinedFilters = require('../controllers/getBooksByCombinedFilters');
 
 
 const bookRouter = Router();
@@ -14,14 +15,9 @@ bookRouter.get("/", async (req, res) => {
     const { title , genre, author } = req.query;
     let books;
     try {
-        if (title){
-            books = await getBookByTitleLike(title)
-        } else if(genre) {
-            books = await getBooksByGenre(genre)
-        } else if(author) {
-            books =  await getBooksByAuthor(author)
-        }
-        else {
+        if(title || genre || author) {
+            books = await getBooksByCombinedFilters(title, genre, author)
+        } else {
             books = await getAllBooks()
         }
         res.status(200).json(books);
